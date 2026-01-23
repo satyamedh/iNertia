581fd587560182221d87590d3560af93008dc173
Satyamedh Hulyalkar <satyamedh@satyamedh.me>
2026-01-23 23:21:27 +0530

    add vector operations and magnitude calculation to Vector3 class

--- maths/Vector3.h
+++ maths/Vector3.h
@@ -14,10 +14,94 @@ namespace iNertia {
         Vector3(): x(0), y(0), z(0) {}
         Vector3(const real x, const real y, const real z): x(x), y(y), z(z) {}
 
+        void operator+=(const Vector3& other) {
+            this->x += other.x;
+            this->y += other.y;
+            this->z += other.z;
+        }
+
+        Vector3 operator+(const Vector3& other) const {
+            return Vector3(x + other.x, y + other.y, z + other.z);
+        }
+
+        void addScaledVector(const Vector3& other, const real scale) {
+            this->x += other.x * scale;
+            this->y += other.y * scale;
+            this->z += other.z * scale;
+        }
+
+        void operator-=(const Vector3& other) {
+            this->x -= other.x;
+            this->y -= other.y;
+            this->z -= other.z;
+        }
+
+        Vector3 operator-(const Vector3& other) const {
+            return Vector3(x - other.x, y - other.y, z - other.z);
+        }
+
+        void operator*=(const real scalar) {
+            this->x *= scalar;
+            this->y *= scalar;
+            this->z *= scalar;
+        }
+
+        Vector3 operator*(const real scalar) const {
+            return Vector3(this->x*scalar, this->y*scalar, this->z*scalar);
+        }
+
+        void componentProductUpdate(const Vector3& other) {
+            this->x *= other.x;
+            this->y *= other.y;
+            this->z *= other.z;
+        }
+
+        real dotProduct(const Vector3& other) const {
+            return this->x*other.x + this->y*other.y + this->z*other.z;
+        }
+
+        real operator*(const Vector3& other) const {
+            return this->x*other.x + this->y*other.y + this->z*other.z;
+        }
+
+        Vector3 crossProduct(const Vector3& other) const {
+            return Vector3(
+                    this->y*other.z - this->z*other.y,
+                    this->z*other.x - this->x*other.z,
+                    this->x*other.y - this->y*other.x
+            );
+        }
+
+        void operator%=(const Vector3& other) {
+            *this = this->crossProduct(other);
+        }
+
+        Vector3 operator%(const Vector3& other) const {
+            return Vector3(
+                    this->y*other.z - this->z*other.y,
+                    this->z*other.x - this->x*other.z,
+                    this->x*other.y - this->y*other.x
+            );
+        }
+
         void invert() {
             x = -x;
             y = -y;
             z = -z;
+        }
+
+        real magnitude() const {
+            return real_sqrt(x*x + y*y + z*z);
+        }
+
+        real squareMagnitude() const {
+            return x*x + y*y + z*z;
+        }
+
+        void normalize() {
+            if (const real len = magnitude(); len > 0) {
+                *this = *this * static_cast<real>(1 / len);
+            }
         }
 
     };
--- maths/precision.h
+++ maths/precision.h
@@ -6,6 +6,8 @@
 #define INERTIA_PRECISION_H
 #include "fix.h"
 
+#define real_sqrt std::sqrtf // will need to fixed-point sqrt implementation later when *optimizing*
+
 namespace iNertia {
     typedef float real; // switch to Fix<8, int32_t> if and when vog allows
 }
