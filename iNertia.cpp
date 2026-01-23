3ef33c024759c522d5a0b19bff1054fe711c91c1
Satyamedh Hulyalkar <satyamedh@satyamedh.me>
2026-01-23 14:47:00 +0530

    Initial commit

---
+++ iNertia.cpp
@@ -0,0 +1,7 @@
+#include "iNertia.h"
+
+#include <iostream>
+
+void hello() {
+    std::cout << "Hello, World!" << std::endl;
+}
---
+++ iNertia.h
@@ -0,0 +1,6 @@
+#ifndef INERTIA_LIBRARY_H
+#define INERTIA_LIBRARY_H
+
+void hello();
+
+#endif // INERTIA_LIBRARY_H
