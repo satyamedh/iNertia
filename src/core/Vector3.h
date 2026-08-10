cce7347a53a2d56bac09bc2c3eafcf00fbdfb7b2
Satyamedh Hulyalkar <satyamedh9@gmail.com>
2026-08-10 23:36:14 +0200

    Add todo to test alignment stuffz

--- src/core/Vector3.h
+++ src/core/Vector3.h
@@ -9,7 +9,7 @@
 namespace iNertia {
     class Vector3 {
     public:
-        real x, y, z; // left out padding; armv5 *shouldn't* benefit from alignment, will test out later tho
+        real x, y, z; // TODO: left out padding; armv5 *shouldn't* benefit from alignment, will test out later tho
 
         Vector3(): x(0), y(0), z(0) {}
         Vector3(const real x, const real y, const real z): x(x), y(y), z(z) {}
